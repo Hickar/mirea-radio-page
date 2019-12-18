6bbaf5ae6ee3e7def19caf58ba853f5c3334021f
Egor Pershin <hickar@icloud.com>
2019-12-18 15:35:54 +0300

    Init commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  resolve: {
+    alias: {
+      Styles: path.resolve(__dirname, 'src/styles/'),
+      Scripts: path.resolve(__dirname, 'src/scripts/'),
+      Images: path.resolve(__dirname, 'src/images/'),
+      Fonts: path.resolve(__dirname, 'src/fonts/'),
+      Pages: path.resolve(__dirname, 'src/pages/')
+    }
+  },
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.css$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [
+        'css-hot-loader',
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+},
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/template.html'
+      }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css',
+    })
+  ]
+}
