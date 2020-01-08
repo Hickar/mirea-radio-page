cbb2e3bc0e0ece6da2c82ef918940c583bd211ba
Egor Pershin <hickar@icloud.com>
2020-01-08 18:11:10 +0300

    Player functionality added

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,56 +1,55 @@
 const player = () => {
 
-    fetch("http://s0.radioheart.ru:8000/json_new.xsl?mount=/RH20507&callback=",
-        {
-            // method: "GET",
-            // headers: {
-            //     'Content-Type': 'text/plain'
-            // }
-        })
-        .then((response) => {
-            let result = response.body;
-            console.log(response.text());
-        });
+    const updateMusic = () => {
+        fetch("http://s0.radioheart.ru:8000/json.xsl?mount=/RH20507")
+            .then((response) => {
+                response.json().then(data => {
+                    let artistName, trackName;
+                    [artistName, trackName] = data.mounts[0].title.split("-");
+                    document.querySelector(".player__trackartist").innerHTML = artistName;
+                    document.querySelector(".player__trackname").innerHTML = trackName;
+                })
+            });
+    };
+    setInterval(updateMusic(), 30000);
 
-
-    function parseMusic(results)
-    {
-        for  (var n in results){
-            var nm = results[n];
-            if(nm["title"]){
-                document.querySelector('.player__trackname').innerHTML = nm["title"];
-            }
+    document.querySelector(".player__playbutton").addEventListener("click", (e) => {
+        if (e.target.dataset.playing === "true") {
+            document.getElementById("pauseIcon").setAttribute("fill", "transparent");
+            document.getElementById("playIcon").setAttribute("fill", "white");
+        } else if (e.target.dataset.playing === "false") {
+            document.getElementById("playIcon").setAttribute("fill", "transparent");
+            document.getElementById("pauseIcon").setAttribute("fill", "white");
         }
-    }
-
+    });
 
 
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
-    const audioElement = document.getElementById('audio');
+    const audioElement = document.getElementById("audio");
     const track = audioContext.createMediaElementSource(audioElement);
     track.connect(audioContext.destination);
 
-    const playButton = document.querySelector('.player__playbutton');
+    const playButton = document.querySelector(".player__playbutton");
 
-    playButton.addEventListener('click', function() {
-        if (audioContext.state === 'suspended') {
+    playButton.addEventListener("click", (e) => {
+        if (audioContext.state === "suspended") {
             audioContext.resume();
         }
 
-        if (this.dataset.playing === 'false') {
+        if (e.target.dataset.playing === "false") {
             audioElement.play();
-            this.dataset.playing = 'true';
-        } else if (this.dataset.playing === 'true') {
+            e.target.dataset.playing = "true";
+        } else if (e.target.dataset.playing === "true") {
             audioElement.pause();
-            this.dataset.playing = 'false';
+            e.target.dataset.playing = "false";
         }
     }, false);
 
     const gainNode = audioContext.createGain();
 
-    const volumeControl = document.querySelector('.volumeslider');
-    volumeControl.addEventListener('input', (e) => {
+    const volumeControl = document.querySelector(".volumeslider");
+    volumeControl.addEventListener("input", (e) => {
         gainNode.gain.value = e.target.value;
     }, false);
     track.connect(gainNode).connect(audioContext.destination);
