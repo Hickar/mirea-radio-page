2ddae9f5442581b6f6ada0cd6db61eb6e1cdd89c
Egor Pershin <hickar@icloud.com>
2020-01-09 02:52:27 +0300

    Dark theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
-import player from 'Scripts/player.js';
+import player from "Scripts/player.js";
+import darkmodeButtonHandler from "Scripts/darkmodeButton.js";
 
 window.onload = () => {
     player();
+    darkmodeButtonHandler();
 };
